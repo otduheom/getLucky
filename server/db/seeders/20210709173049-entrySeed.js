63b49ec1b8d98fa7884e6ababbf46827608b3470
otduheom <vldpautov@mail.ru>
2026-01-11 15:30:59 +0300

    fix bd

--- server/db/seeders/20210709173049-entrySeed.js
+++ server/db/seeders/20210709173049-entrySeed.js
@@ -4,18 +4,29 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert(
-      'Users',
-      [
-        {
-          name: 'admin',
-          email: 'admin@mail.ru',
-          password: await bcrypt.hash('Qwerty1!', 10),
-          isAdmin: true,
-        },
-      ],
-      {},
+    // Проверяем, существует ли уже пользователь с таким email
+    const existingUsers = await queryInterface.sequelize.query(
+      `SELECT id FROM "Users" WHERE email = 'admin@mail.ru' LIMIT 1;`,
+      { type: Sequelize.QueryTypes.SELECT }
     );
+
+    // Если пользователь не существует, создаем его
+    if (!existingUsers || existingUsers.length === 0) {
+      await queryInterface.bulkInsert(
+        'Users',
+        [
+          {
+            name: 'admin',
+            email: 'admin@mail.ru',
+            password: await bcrypt.hash('Qwerty1!', 10),
+            isAdmin: true,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        {},
+      );
+    }
   },
 
   async down(queryInterface) {
